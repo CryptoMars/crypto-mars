41103555bd68d84758a17434610ea5e9407a56d5
soerenmaucher <soeren_maucher@web.de>
2021-04-10 16:34:23 +0200

    php formular
    php formular

--- comingsoon/version2/contact-form-handler.php
+++ comingsoon/version2/contact-form-handler.php
@@ -1,34 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta
+      name="description"
+      content="Web site created using create-react-app"
+    />
+    <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+    <!--
+      manifest.json provides metadata used when your web app is installed on a
+      user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
+    -->
+    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+
+
+<body>
+  <h1 style= "align:center; color:black; "><b>Thank you!</b></h1>
+
 <?php
-$errors = '';
-$myemail = 'to-the-mars@web15.s171.goserver.host';//<-----Put Your email address here.
-if(empty($_POST['message']))
-{
-    $errors .= "\n Error: all fields are required";
-}
 
+  $to = "soeren_maucher@web.de";
+  $subject ="neue anmeldung";
+  $email = $_POST['email'];
+  $wallet = $_POST['wallet'];
+  $txt =   $email ."\n ".$wallet;
 
-$email = $_POST['email'];
-$address = $_POST['address'];
-
-
-
-if( empty($errors))
-
-{
-
-$to = $myemail;
-
-$email_subject = "Mars Claim!:";
-
-$email_body = "$email" + "$address" ;
-
-$headers = "From: to-the-mars";
-
-
-mail($to,$email_subject,$email_body,$headers);
-
+mail($to,$subject,$txt);
 //redirect to the 'thank you' page
-header('Location: thanks.html');
-}
 ?>
-
+</body>
+</html>
