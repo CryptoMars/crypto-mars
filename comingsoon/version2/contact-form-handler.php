86337a4ca83e2e070f3c771f8904372319182d15
soerenmaucher <soeren_maucher@web.de>
2021-04-10 17:01:44 +0200

    php form test

--- comingsoon/version2/contact-form-handler.php
+++ comingsoon/version2/contact-form-handler.php
@@ -1,35 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#000000" />
-    <meta
-      name="description"
-      content="Web site created using create-react-app"
-    />
-    <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-    <!--
-      manifest.json provides metadata used when your web app is installed on a
-      user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
-    -->
-    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>To-the-mars!</title>
+<!--
 
+Highway Template
 
+https://templatemo.com/tm-520-highway
+
+-->
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="apple-touch-icon.jpg">
+    <link rel="icon" type="image/png" href="img/favicon.ico"/>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/fontAwesome.css">
+    <link rel="stylesheet" href="css/light-box.css">
+    <link rel="stylesheet" href="css/templatemo-style.css">
+    <link href="https://fonts.googleapis.com/css?family=Kanit:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700&family=Zilla+Slab+Highlight&display=swap" rel="stylesheet">
+    <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+</head>
 <body>
-  <h1 style= "align:center; color:black; "><b>Thank you!</b></h1>
+  <div id="video-container">
+      <div style="z-index:-2;"class="video-overlay"></div>
+      <center>
+        <div style="padding-top:50px;">
+          <h1 style= "align:center; color:white; "><b>Thank you!</b></h1>
+          <a href="/index.html" ><h2 style= "align:center; color:white; "><b>Go back to landing page</b></h2></a>
+        </div>
+      </center>
+  </div>
 
 <?php
-
-  $to = "soeren_maucher@web.de";
+  $to = "dividemars@protonmail.com";//dividemars@protonmail.com
   $subject ="neue anmeldung";
   $email = $_POST['email'];
   $wallet = $_POST['wallet'];
-  $txt =   $email ."\n ".$wallet;
+  $txt =   "email: ".$email ."\n ". "address: ".$wallet;
 
-mail($to,$subject,$txt);
-//redirect to the 'thank you' page
+
+  if ($email !="" & $wallet !=""){
+    mail($to,$subject,$txt);
+  }
+
+
 ?>
 </body>
 </html>
